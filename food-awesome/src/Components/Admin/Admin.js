6ec5e4be925b418fc58cd76db3f6d4c74eb60e62
PGY <PGY@PGYs-MacBook-Air.local>
2017-06-30 16:10:38 +0800

    added bootstrap and react-router

---
+++ food-awesome/src/Components/Admin/Admin.js
@@ -0,0 +1,14 @@
+import React, {Component, propTypes} from 'react';
+
+export default class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>MyComponent</div>);
+  }
+}
+
+.propTypes = {
+};
