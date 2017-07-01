c45dbbf13b436a8de20b5e8019f81926ef391f5e
dheamariesta <dhea23mar@gmail.com>
2017-07-01 10:43:04 +0800

    fix issue with app.js

--- food-awesome/src/Components/App/App.js
+++ food-awesome/src/Components/App/App.js
@@ -1,12 +1,26 @@
+
+// Import modules
 import React, { Component } from 'react';
-import Header from '../Header/Header';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+//Import Components
+import Header from "../Header/Header";
+//import Admin from "../Admin/Admin";
+
+// Import Static Files
 import './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+
       </div>
     );
   }
