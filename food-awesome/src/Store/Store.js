de8e9f78ca5cdac9e381c94c3880713966466f24
PGY <PGY@PGYs-MacBook-Air.local>
2017-07-04 11:31:17 +0800

    yarn add and added code for REDUX

---
+++ food-awesome/src/Store/Store.js
@@ -0,0 +1,17 @@
+import { createStore, compose, combineReducers } from 'redux'; // compose is used for debugging here
+import AdminReducer from '../Reducers/Admin';
+
+export let initStore = () => {
+
+  const reducer = combineReducers({
+    admin: AdminReducer               // declaring all reducer into one object
+    // example: user: UserReducer
+  });
+
+  const store = createStore( reducer,     // passing all reducer-- each reducer creates as an array inside this.props
+    compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f      // f is just a way to do nothing
+  ) ) 
+
+  return store;
+}
--- food-awesome/src/index.js
+++ food-awesome/src/index.js
@@ -6,5 +6,22 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { Provider } from 'react-redux';   // yarn add react-redux
+import { initStore } from './Store/Store';  //calling initStore function from Store file, already have createStore inside
+//API
+import {setAdmin, getAdmin} from './API/AdminAPI';
+
+const store = initStore();
+
+store.subscribe( () => {
+  const state = store.getState();
+  setAdmin(state.admin);
+})
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
