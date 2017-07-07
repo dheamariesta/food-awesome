556f3a016289c6e92d206e18ba60a3c37c03848b
dheamariesta <dhea23mar@gmail.com>
2017-07-07 16:29:11 +0800

    Add mapPropstoState in RestaurantItems

--- food-awesome/src/Store/Store.js
+++ food-awesome/src/Store/Store.js
@@ -6,12 +6,13 @@ export let initStore = () => {
   const reducer = combineReducers({
     admin: AdminReducer               // declaring all reducer into one object
     // example: user: UserReducer
+
   });
 
   const store = createStore( reducer,     // passing all reducer-- each reducer creates as an array inside this.props
     compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f      // f is just a way to do nothing
-  ) ) 
+  ) )
 
   return store;
 }
