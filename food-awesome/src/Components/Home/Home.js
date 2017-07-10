4518ca8a94a9343a2b7719631f08bdebcebff874
Bryan Lee <bryan.lee.jh@gmail.com>
2017-07-10 11:39:06 +0800

    testing login

--- food-awesome/src/Components/Home/Home.js
+++ food-awesome/src/Components/Home/Home.js
@@ -1,21 +1,45 @@
 import React, {Component} from 'react';
 import Header from '../Header/Header'
 import HomeContainer from './HomeContainer/HomeContainer';
+import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import { connect } from 'react-redux';
+
 export class Home extends Component {
   // constructor(props) {
   //   super(props);
   // }
 
   render() {
+    const isLoggedIn = this.props.user._id;
+
     return (
       <div className="container-fluid">
         <Header/>
-        <HomeContainer/>
+          {isLoggedIn ? (
+            <div className="isLoggedIn">
+              <HomeContainer/>
+            </div>
+          ) : (
+            <div className="col-md-4 col-md-offset-4" id="Login">
+              <Login/>
+            </div>
+          )}
         <Footer/>
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
