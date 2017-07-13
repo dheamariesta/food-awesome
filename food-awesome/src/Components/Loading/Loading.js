dc790fed4307eb689a8f18c5a440c7fed98183ae
Bryan Lee <bryan.lee.jh@gmail.com>
2017-07-13 10:12:31 +0800

    test commit create loading component

---
+++ food-awesome/src/Components/Loading/Loading.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+export class Loading extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount(){
+
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <i className="fa fa-spin fa-3x fa-circle-o-notch" aria-hidden="true" id="loadingSpinner" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Loading;
