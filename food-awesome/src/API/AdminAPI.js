f1c44b6145c484b248bb3978e7c612f93710d389
Wiz1221 <geokyan89@gmail.com>
2017-07-13 12:28:39 +0800

    Update AdminAPI.js

--- food-awesome/src/API/AdminAPI.js
+++ food-awesome/src/API/AdminAPI.js
@@ -15,9 +15,35 @@ export const getAdmin = () => {
   return Array.isArray(restaurants) ? restaurants: [];
 }
 
-// export const searchReviewInUser = (reviews,nameToSearch) => {
-//   reviewsToShow = reviews.filter((elem,index) => {
-//     return elem.title.includes(nameToSearch);
-//   })
-//   return reviewsToShow;
-// }
+export const searchFunction = (thingToSearchIn,nameToSearch) => {
+  let thingToShow = thingToSearchIn.filter((elem,index) => {
+    return elem.title.includes(nameToSearch);
+  })
+  return thingToShow;
+}
+
+export const calculateStar = (restaurant,postUpdateOrDelete,newStar,oldStar) => {
+  console.log(restaurant)
+  let total = restaurant.star * restaurant.reviews.length + 1 ;
+  console.log(total)
+  let average;
+  switch (postUpdateOrDelete) {
+    case "POST":
+      total += newStar
+      console.log(total)
+      average = total/(restaurant.reviews.length + 2);
+      break;
+    case "UPDATE":
+      total -= oldStar;
+      total += newStar;
+      average = total/(restaurant.reviews.length);
+      break;
+    case "DELETE":
+      total -= oldStar;
+      average = total/(restaurant.reviews.length - 1);
+      break;
+    default:
+      console.log("wrong type")
+  }
+  return average
+}
