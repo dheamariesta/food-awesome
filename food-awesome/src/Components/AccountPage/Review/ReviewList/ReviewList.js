c19f4149e9ff7b8a0c9c7827a415442543d2406e
Wiz1221 <geokyan89@gmail.com>
2017-07-13 13:00:05 +0800

    Update ReviewList.js

--- food-awesome/src/Components/AccountPage/Review/ReviewList/ReviewList.js
+++ food-awesome/src/Components/AccountPage/Review/ReviewList/ReviewList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import ReviewListView from '../ReviewListView/ReviewListView';
-import { searchFunction } from '../../../../API/adminAPI';
+import { searchFunction } from '../../../../API/AdminAPI';
 
 
 class ReviewList extends Component {
